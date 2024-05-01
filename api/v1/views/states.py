275888e343e3cec6360e42be0a55f41d6a1e8059
abeni-al7 <abenezeralebachew3@gmail.com>
2024-05-02 00:57:01 +0300

    Correct get

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -15,7 +15,7 @@ def get_states():
         state_list.append(state.to_dict())
     return jsonify(state_list)
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<string:state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """Retrieves a State object"""
     state = storage.get("State", state_id)
@@ -23,7 +23,7 @@ def get_state(state_id):
         abort(404)
     return jsonify(state.to_dict())
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<string:state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     """Deletes a State object"""
     state = storage.get("State", state_id)
@@ -44,7 +44,7 @@ def post_state():
     state.save()
     return jsonify(state.to_dict()), 201
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/states/<string:state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
     """Updates a State object"""
     state = storage.get("State", state_id)
