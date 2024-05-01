61c186acc1c67ba14bcd4477cf6b47983ac7dccd
abeni-al7 <abenezeralebachew3@gmail.com>
2024-05-01 22:05:11 +0300

    teardown

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A flask app"""
-from flask import Flask, jsonify
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -8,7 +8,7 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-@app.teardown.appcontext
+@app.teardown_appcontext
 def teardown_db():
     """teardown"""
     storage.close()
@@ -17,4 +17,3 @@ if __name__ == "__main__":
     HBNB_API_HOST = getenv('HBNB_API_HOST', default='0.0.0.0')
     HBNB_API_PORT = getenv('HBNB_API_PORT', default='5000')
     app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
-
