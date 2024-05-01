0e7a12ed993614dfe74c8204a46f954a7a61c392
abeni-al7 <abenezeralebachew3@gmail.com>
2024-05-01 23:38:54 +0300

    404 json

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A flask app"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -13,7 +13,12 @@ def teardown_db(error):
     """teardown"""
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """Return a custom 404 message"""
+    return jsonify({"error": "Not found"})
+
 if __name__ == "__main__":
     HBNB_API_HOST = getenv('HBNB_API_HOST', default='0.0.0.0')
     HBNB_API_PORT = getenv('HBNB_API_PORT', default='5000')
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True, debug=True)
